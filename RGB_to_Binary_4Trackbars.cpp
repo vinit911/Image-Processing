98a5c6155b4e9d35b61e0e2ff9c6ad0d37538b0c
Vinit Kumar Singh <vinitsingh911@gmail.com>
2017-10-26 11:47:58 +0530

    Update and rename 3Trackbar.cpp to RGB_to_Binary_4Trackbars.cpp

--- RGB_to_Binary_4Trackbars.cpp
+++ RGB_to_Binary_4Trackbars.cpp
@@ -1,3 +1,4 @@
+//Converting an RGB image to binary using four trackbars to control R,G,B colour intensities and also the error range
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/core/core.hpp"
